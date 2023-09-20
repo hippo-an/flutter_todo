db1a2dab8a7ea5b63594c130172ac02df3f5dcc8
ansehyeong <dkstpgud@gmail.com>
2023-09-21 02:34:13 +0900

    feat: task list provider 에 업데이트 로직 추가 - 근데 task 업데이트를 task list provider 에서 하는게 맞을까? 하나의 task 를 업데이트 하는 경우 태스크를 사용하는 다른 프로바이더에서는 어떻게 업데이트한 상태를 유지할 수 있을까

--- lib/provider/task_list_provider.dart
+++ lib/provider/task_list_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:todo_todo/consts/tools.dart';
 import 'package:todo_todo/models/category_model.dart';
@@ -10,19 +12,25 @@ class TaskListProvider extends ChangeNotifier {
   List<TaskModel> tasks(CategoryModel? categoryModel) {
     if (categoryModel == null) {
       final copiedList = [..._tasks];
-      copiedList.sort((a, b) {return 0;});
+      copiedList.sort((a, b) {
+        return 0;
+      });
       return copiedList;
     }
 
-    final categorizedTask = _tasks.where((task) => task.categoryModel == categoryModel).toList();
-    categorizedTask.sort((a, b) {return 1;});
+    final categorizedTask =
+        _tasks.where((task) => task.categoryModel == categoryModel).toList();
+    categorizedTask.sort((a, b) {
+      return 1;
+    });
     return categorizedTask;
   }
 
-  void createTask(String name, {
+  void createTask(
+    String name, {
     DateTime? dueDate,
     CategoryModel? categoryModel,
-    List<SubTaskModel> subTasks = const[],
+    List<SubTaskModel> subTasks = const [],
   }) {
     final now = DateTime.now();
     final task = TaskModel(
@@ -32,14 +40,43 @@ class TaskListProvider extends ChangeNotifier {
         dueDate: dueDate,
         createdAt: now,
         updatedAt: now,
-        subTasks: subTasks
-    );
+        subTasks: subTasks);
 
     _tasks.add(task);
 
     notifyListeners();
   }
 
-  void updateTick(String taskId) {
+  TaskModel? updateTask({
+    required TaskModel task,
+    String? taskName,
+    bool? isDone,
+    String? note,
+    CategoryModel? categoryModel,
+    DateTime? dueDate,
+    List<SubTaskModel>? subTasks,
+    File? attachment,
+  }) {
+    final index = _tasks.indexOf(task);
+    if (index < -1) {
+      return null;
+    }
+    final updatedTask = task.copyWith(
+        taskName: taskName,
+        isDone: isDone,
+        note: note,
+        categoryModel: categoryModel,
+        dueDate: dueDate,
+        subTasks: subTasks,
+        attachment: attachment,
+        updatedAt: DateTime.now());
+
+    _tasks
+      ..removeAt(index)
+      ..insert(index, updatedTask);
+
+    notifyListeners();
+
+    return updatedTask;
   }
 }
